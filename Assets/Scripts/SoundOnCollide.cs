f28c372c965e67fb023c8e3c20a044568f287337
Kobe Change <kobe@Kobes-MacBook-Pro-2.local>
2019-08-29 11:28:52 -0700

    adds sound

---
+++ Assets/Scripts/SoundOnCollide.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundOnCollide : MonoBehaviour
+{
+    public string[] sounds;
+    int[] FileIDs;
+    int SoundID;
+    private void Awake()
+    {
+        AndroidNativeAudio.makePool();
+        FileIDs = new int[sounds.Length];
+
+        for(int i = 0; i < FileIDs.Length; i++)   
+        {
+            string s = sounds[i];
+            FileIDs[i] = AndroidNativeAudio.load(s);
+        }
+    
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        // Play native audion
+        var i = Random.Range(0, FileIDs.Length);
+        AndroidNativeAudio.play(FileIDs[i]);
+    }
+
+    void OnApplicationQuit()
+    {
+        // Clean up when done
+        foreach (int FileID in FileIDs)
+        {
+            AndroidNativeAudio.unload(FileID);
+        }
+        AndroidNativeAudio.releasePool();
+    }
+
+}
